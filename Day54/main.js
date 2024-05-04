bc105b13b72901597dabe420488fed388eb9afab
zain <zainmd.htm@gmail.com>
2024-05-04 17:58:23 +0500

    Add files

---
+++ Day54/main.js
@@ -0,0 +1,25 @@
+//1.
+let admain;
+let name;
+
+name = 'John';
+
+admain = name;
+
+alert(admain)
+
+2.//
+let ourPlanet = 'Earth';
+
+let currentUserName = 'John';
+
+3.//
+const BIRTHDAY = '18.4.1982';
+
+const age = someCode(BIRTHDAY)
+
+
+
+
+
+
