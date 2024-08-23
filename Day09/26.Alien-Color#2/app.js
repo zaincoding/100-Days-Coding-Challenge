ff4f5bf2ae22b5c82d3e76d60d1ccf346315f346
Zain Khan <105432110+zaincoding@users.noreply.github.com>
2024-08-23 12:00:23 +0500

    Update app.js

--- Day09/26.Alien-Color#2/app.js
+++ Day09/26.Alien-Color#2/app.js
@@ -1,17 +1,14 @@
-//Exercise 26: Alien-color2
-//Alien colors
-var Alien_color = ['Red', 'Green', 'Yellow'];
-//if the alien color is green run the if code.
-if (Alien_color.includes('Red')) {
-    console.log("The player is earned 5 points.");
+var Alien_color = "green";
+if (Alien_color == "green") {
+    console.log("The player just earned 5 points.");
 }
 else {
-    console.log("The player is earned 10 points.");
+    console.log("The player just earn 10 points.");
 }
-//if the alien isn't Green run the if code otherwise run the else code.
-if (!Alien_color.includes('Green')) {
-    console.log("The player is earned 5 points.");
+Alien_color = "yellow";
+if (Alien_color == "green") {
+    console.log("The player just earned 5 points.");
 }
 else {
-    console.log("The player is earned 10 points.");
+    console.log("The player just earned 10 points.");
 }
