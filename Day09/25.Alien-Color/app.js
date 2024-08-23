ce814a8588ae9b0fe042013a56d543069ad1142e
Zain Khan <105432110+zaincoding@users.noreply.github.com>
2024-08-23 11:56:43 +0500

    Update app.js

--- Day09/25.Alien-Color/app.js
+++ Day09/25.Alien-Color/app.js
@@ -1,9 +1,8 @@
-//Exercise 25: Alien Color.
-var Alien = ['Green', 'Yellow', 'Red'];
-//if the alien color is green run the block code.
-if ((Alien.includes('Green'))) {
-    console.log("The player earned 5 points");
+var Alien_color = "Green";
+if (Alien_color == "Green") {
+    console.log("The player just earned 5 points.");
 }
-if (!Alien.includes('Green')) {
-    console.log('The player earned 10 points.');
+Alien_color = "Red";
+if (Alien_color == "Green") {
+    console.log("The player just earned 5 points.");
 }
