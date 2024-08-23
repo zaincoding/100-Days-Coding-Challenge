222db40924ad4ac4974b5c4aeffd41fc0c51295b
Zain Khan <105432110+zaincoding@users.noreply.github.com>
2024-08-23 12:01:04 +0500

    Update app.ts

--- Day09/26.Alien-Color#2/app.ts
+++ Day09/26.Alien-Color#2/app.ts
@@ -1,17 +1,17 @@
-// let Alien_color = "green"
+let Alien_color = "green"
     
 
-// if(Alien_color == "green" ){
-//    console.log("The player just earned 5 points.")
-// }
-// else{
-//    console.log("The player just earn 10 points.")
-// }
+if(Alien_color == "green" ){
+   console.log("The player just earned 5 points.")
+}
+else{
+   console.log("The player just earn 10 points.")
+}
 
-//   Alien_color = "yellow";
-// if(Alien_color == "green" ){
-//     console.log("The player just earned 5 points.")
-//  }
-//  else{
-//    console.log("The player just earned 10 points.")
-//  }
+  Alien_color = "yellow";
+if(Alien_color == "green" ){
+    console.log("The player just earned 5 points.")
+ }
+ else{
+   console.log("The player just earned 10 points.")
+ }
